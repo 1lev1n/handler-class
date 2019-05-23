373712c665fe0d951b5e4f7075a8276a77bcaa0d
1lev1n <50931223+1lev1n@users.noreply.github.com>
2019-05-23 19:22:57 -0300

    Ligar aqui

---
+++ app.js
@@ -0,0 +1,4 @@
+const { login } = require("./src/Client"); // Puxando a classe Client com a propriedade 'login' do cliente do discordjs
+
+login('token'); // Logando na conta
+// Já vai executar os métodos 'addCommands()' e 'addEvents()'
